395ff8bb2005f276af21f511caf5db0c7a8db9c1
Nachiket Parjane <nachiketparjane054@gmail.com>
2023-06-12 23:09:14 +0530

    created base fpr application

--- adminpage/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import edittor,draft,herosection,herosectionnews,AdvertisementManger
-
-# admin.site.register(edittor)
-admin.site.register(draft)
-admin.site.register(herosection)
-admin.site.register(herosectionnews)
-admin.site.register(AdvertisementManger)
